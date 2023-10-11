e0b260c30d620940f9a4fb74913fc50e4a4d691e
yusufburakkaradag42 <yusufburakkaradag42@gmail.com>
2023-10-11 15:12:30 +0300

    use Reducer kullanımı

--- App.js
+++ App.js
@@ -1,31 +1,51 @@
 
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { StyleSheet, View ,Button ,Text } from 'react-native';
 
+const reducer = (state, action) => {
+  //   console.log(action);
+  switch (action.type) {
+    case 'increment':
+      return { ...state, count: state.count + action.payload };
+    case 'decrement':
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};//state == {count:0}
+//action=={type:'increment' || type:'decrement' ,payload}
 export default function App() {
 
-  const[counter , setCounter] = useState(0)
-
-
-  return (
-    <View >
-     <Button 
-      onPress={()=>{
-        setCounter(counter+1)
-      }}
-    title='artır'
-     />
-        <Button 
-      onPress={()=>{
-        setCounter(counter-1)
-      }}
-    title='azalt'
-     />
-     <Text >Sayı: {counter}</Text>
-    </View>
+   //   const [counter, setCounter] = useState(0);
+   const [state, dispatch] = useReducer(reducer, { count: 0 });
+   return (
+     <View style={styles.container}>
+       <Button
+      
+         title="Arttır"
+         onPress={() => {
+           dispatch({ type: 'increment', payload: 1 });
+           //   setCounter(counter + 1);
+         }}
+       />
+       <Button
+         title="Azalt"
+         onPress={() => {
+           dispatch({ type: 'decrement', payload: 1 });
+           //   setCounter(counter - 1);
+         }}
+       />
+       <Text>Sayı: {state.count}</Text>
+     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    
+  },
 
 });
